48aa53c56eeb42ee24cb82eff6c820af224c8b0c
LucyChC <lucindachc10@gmail.com>
2025-06-10 14:25:22 -0600

    Mejoras en el rol de agregar usuario y en la vista de telefono usuario

--- src/app/models/telefonoUsuario.ts
+++ src/app/models/telefonoUsuario.ts
@@ -3,6 +3,10 @@ export class TelefonoUsuario {
     public idTelefonoUsuario: number = 0,
     public idUsuario: number = 0,
     public tipoTel: string = '',
-    public telefono: string = ''
+    public telefono: string = '',
+    public usuario?: {
+      nombre: string;
+      apellido: string;
+    }
   ) {}
 }
